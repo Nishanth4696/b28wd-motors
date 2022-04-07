6289993d77fab4ecc7418ebaddc8f01e1317aee4
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-07 12:56:42 +0530

    commit

--- client/src/components/SharedLayout.js
+++ client/src/components/SharedLayout.js
@@ -20,7 +20,7 @@ class SharedLayout extends Component{
               <div className="row">
                 <div className="col-sm-12 col-md-12 col-lg-12">
                   <div className="banner-item banner-2x no-bg color-inher">
-                    <h2 className="f-weight-300">More than <strong>20 000 </strong>Vehicle avaiable</h2>
+                    <h2 className="f-weight-300">More than <strong>20 000 </strong>Vehicle available</h2>
                     <p></p>			
                     <Link to={"/cars"} className="ht-btn ht-btn-default ht-btn-2x m-t-lg-35">          
                       View all cars                      
