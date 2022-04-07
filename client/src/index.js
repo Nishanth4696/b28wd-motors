f43cbadeceadff3fb6810fd8e6147fd3626ea66d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-04-07 12:48:14 +0530

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-
+// import { LoginApp } from './LoginApp';
 import App from './components/App';
 import reducers from './reducers';
 
